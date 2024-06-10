39aeab22f8f1355dcde7655aff04874e3c057163
MiguelWeed <miguelmem12@gmail.com>
2024-06-10 02:36:25 +0000

    adicionadas as funções do botão de próxima pergunta e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -99,7 +99,7 @@ function montarPergunta() {
                     </label>
                 </form>
 
-                <button>Enviar</button>
+                <button>Responder</button>
             </section>
     `
 }
@@ -117,20 +117,41 @@ function guardarResposta(evento) {
 }
 
 function validarResposta() {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+    botaoEnviar.addEventListener("click", proximaPergunta)
+
+    if (pergunta === 10) {
+        botaoEnviar.innerHTML = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
     if (resposta === quiz.questions[pergunta-1].anwser) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
     } else {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
-        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
+    pergunta = pergunta + 1
 }
 
-async function iniciar() {
-    alterarAssunto()
-    await buscarPerguntas()
-    montarPergunta()
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
 
+    window.location.href = "../resultado/resultado.html"
+}
+
+function proximaPergunta() {
+    montarPergunta()
+    adicionarEventoInputs()
+}
+
+function adicionarEventoInputs() {
     const inputsRespostas = document.querySelectorAll(".alternativas input")
     inputsRespostas.forEach(input => {
         input.addEventListener("click", guardarResposta)
@@ -139,6 +160,13 @@ async function iniciar() {
             respostaCorretaId = input.id
         }
     })
+}
+
+async function iniciar() {
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
 }
 
 iniciar()
